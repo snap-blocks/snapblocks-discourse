4ff17b60a5734030a0108b7c59a555f98a1089cc
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-12-06 21:09:45 -0700

    update to snapblocks 1.7.0

--- assets/javascripts/lib/snapblocks/translations-all-es.js
+++ assets/javascripts/lib/snapblocks/translations-all-es.js
@@ -1,9 +1,9 @@
 /**
- * snapblocks v1.6.1
+ * snapblocks v1.7.0
  * https://snap-blocks.github.io/
  * Make pictures of Snap blocks from text. This is a fork of Scratchblocks.
  *
- * Copyright 2013-2021, Tim Radvan
+ * Copyright 2013-2024, Tim Radvan
  * Copyright 2024-2024, ego-lay-atman-bay
  * @license MIT
  */
